1437e0365bcc2f85a9a3ecfafb27db37d8932884
Whirlwind <1585819425@qq.com>
2021-06-17 23:49:09 +0800

    mail

--- src/test/java/com/whirlwind/gxmubbs/MyTest.java
+++ src/test/java/com/whirlwind/gxmubbs/MyTest.java
@@ -1,13 +1,27 @@
 package com.whirlwind.gxmubbs;
 
+import com.whirlwind.gxmubbs.util.MailClient;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
 
 import java.util.Date;
 
+@SpringBootTest
+@ContextConfiguration(classes=GxmubbsApplication.class)
 public class MyTest {
     @Test
     public void testDate(){
         Date date=new Date();
         System.out.println(date);
+    }
+
+    @Autowired
+    private MailClient mailClient;
+
+    @Test
+    public void testTextMail(){
+        mailClient.sendMail("1449979716@qq.com","Test","这是一条测试");
     }
 }
